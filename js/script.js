c5e73849f2a9cd492d93aac2bb53411d61b1d717
silke1102 <48947166+silke1102@users.noreply.github.com>
2020-04-17 13:48:30 +0200

    made my rocket!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+var doCoolStuff = function(){
+	var currentClassName = document.getElementById('cool').className;
+	if (currentClassName == 'cool'){
+		document.getElementById('cool').className = 'cool-red';
+	} else{
+		document.getElementById('cool').className = 'cool';
+	}
+};
+
+var sayMyName = function (name){
+	alert('my name is: ' + name);
+};
+
 var car = {
 		make: 'VW',
 		type: 'polo',
